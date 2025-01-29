146e2ae3339eff74be3649cfe057cf90057ab863
Mehebul02 <mehebul2123@gmail.com>
2025-01-30 00:51:08 +0600

    Install the zod form

--- src/pages/admin/academic-Management/CreateAcademicSemester.tsx
+++ src/pages/admin/academic-Management/CreateAcademicSemester.tsx
@@ -2,42 +2,29 @@ import { Button, Col, Flex } from "antd";
 import PhForm from "../../../components/form/PhForm";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PhSelectForm from "../../../components/form/PhSelectForm";
+import { semesterOptions } from "../../../constrants/semester";
+import { monthOptions } from "../../../constrants/global";
 
-const nameOptions =[
-    {
-        value:'01',
-        label:'Autumn'
-    },
-    {
-        value:'02',
-        label:'Summer'
-    },
-    {
-        value:'03',
-        label:'Fall'
-    },
-   
-]
 
 const currentYear = new Date().getFullYear()
 const yearOptions =[0,1,2,3,4].map(number=>({
     value:currentYear+number,
     label:currentYear+number
 }))
-console.log(yearOptions);
-console.log(currentYear);
-
 
 const CreateAcademicSemester = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        console.log(data.name)
         // const name =nameOptions.find((item)=>item.value === data.name)
-        const name =nameOptions[Number(data.name)-1].label
+        const name =semesterOptions[Number(data.name)-1].label
         const semesterData ={
             name,
-            label:data.name
+            code:data.name,
+            year:data.year
+
         }
         console.log(semesterData);
+
+        console.log(data);
     }
 
 
@@ -47,10 +34,10 @@ const CreateAcademicSemester = () => {
             <Col span={4}>
                 <PhForm onSubmit={onSubmit}>
 
-                    <PhSelectForm label='Name' name='name' options={nameOptions} defaultValue='Autumn'/>
+                    <PhSelectForm label='Name' name='name' options={semesterOptions} defaultValue='Autumn'/>
                     <PhSelectForm label='Year' name='year' options={yearOptions} defaultValue={currentYear} />
-                    <PhSelectForm label='Start Month' name='startMonth' options={nameOptions} />
-                    <PhSelectForm label='End Month' name='endMonth' options={nameOptions} />
+                    <PhSelectForm label='Start Month' name='startMonth' options={monthOptions} />
+                    <PhSelectForm label='End Month' name='endMonth' options={monthOptions} />
                 
                     <Button htmlType="submit">Submit</Button>
                 </PhForm>
