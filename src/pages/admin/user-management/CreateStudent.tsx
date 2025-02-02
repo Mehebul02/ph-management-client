986e0ba078f23979f93217118c854471a2cd850d
Mehebul02 <mehebul2123@gmail.com>
2025-02-02 15:52:44 +0600

    Create academic semester

--- src/pages/admin/user-management/CreateStudent.tsx
+++ src/pages/admin/user-management/CreateStudent.tsx
@@ -5,6 +5,7 @@ import { Button, Col, Divider, Row } from "antd";
 import PhSelectForm from "../../../components/form/PhSelectForm";
 import { bloodGroupOptions, gendersOptions } from "../../../constrants/global";
 import PHDatePicker from "../../../components/form/PhDatePicker";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.Api";
 
 
 
@@ -46,6 +47,13 @@ const studentDummyData = {
 }
 
 const CreateStudent = () => {
+    const { data: sSemesterData, isLoading } = useGetAllSemestersQuery(undefined)
+
+    const semesterOptions = sSemesterData?.data?.map((item) => ({
+        value: item._id,
+        label:`${item.name} ${item.year}`,
+    }))
+
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         const formData = new FormData();
         formData.append('data', JSON.stringify(data));
@@ -58,12 +66,43 @@ const CreateStudent = () => {
         // console.log(Object.fromEntries(formData));
     };
 
-   
+    const studentDefaultVale = {
+        "name": {
+            "firstName": "I am ",
+            "middleName": "Student",
+            "lastName": "Number 1"
+        },
+        "gender": "male",
+        // "dateOfBirth": "1990-01-01",
+        "bloogGroup": "A+",
+
+        "email": "student2@gmail.com",
+        "contactNo": "1235678",
+        "emergencyContactNo": "987-654-3210",
+        "presentAddress": "123 Main St, Cityville",
+        "permanentAddress": "456 Oak St, Townsville",
+
+        "guardian": {
+            "fatherName": "James Doe",
+            "fatherOccupation": "Engineer",
+            "fatherContactNo": "111-222-3333",
+            "motherName": "Mary Doe",
+            "motherOccupation": "Teacher",
+            "motherContactNo": "444-555-6666"
+        },
+        "localGuardian": {
+            "name": "Alice Johnson",
+            "occupation": "Doctor",
+            "contactNo": "777-888-9999",
+            "address": "789 Pine St, Villageton"
+        },
+    }
+
 
     return (
         <Row gutter={20}>
             <Col span={24}>
-                <PhForm onSubmit={onSubmit}>
+                <PhForm onSubmit={onSubmit} defaultValues={studentDefaultVale}>
                     <Divider>
                         Personal info
                     </Divider>
@@ -82,7 +121,7 @@ const CreateStudent = () => {
                         </Col>
                         <Col xs={24} sm={12} md={8}>
                             {/* <PHInputForm type="date" name="dateOfBirth" label="Date Of Birth" /> */}
-                            <PHDatePicker name="dateOfBirth" label="Date Of Birth"/>
+                            <PHDatePicker name="dateOfBirth" label="Date Of Birth" />
                         </Col>
                         <Col xs={24} sm={12} md={8}>
                             <PhSelectForm options={bloodGroupOptions} name="bloogGroup" label="Blood Group" />
@@ -90,7 +129,7 @@ const CreateStudent = () => {
                         {/* Add more fields as needed */}
                     </Row>
                     <Divider>
-                       Contact info
+                        Contact info
                     </Divider>
                     <Row gutter={20}>
                         <Col xs={24} sm={12} md={8}>
@@ -100,17 +139,70 @@ const CreateStudent = () => {
                             <PHInputForm type="number" name="contactNo" label="Contact" />
                         </Col>
                         <Col xs={24} sm={12} md={8}>
-                            <PHInputForm type="emergencyContactNo" name="emergencyContactNo" label="Emergency Contact" />
+                            <PHInputForm type="number" name="emergencyContactNo" label="Emergency Contact" />
                         </Col>
                         <Col xs={24} sm={12} md={8}>
-                            <PHInputForm type="presentAddress" name="presentAddress" label="Present Address" />
+                            <PHInputForm type="text" name="presentAddress" label="Present Address" />
                         </Col>
                         <Col xs={24} sm={12} md={8}>
-                            <PHInputForm type="permanentAddress" name="permanentAddress" label="Permanent Address" />
+                            <PHInputForm type="text" name="permanentAddress" label="Permanent Address" />
                         </Col>
-                        
-                        
-                        {/* Add more fields as needed */}
+                    </Row>
+                    <Divider>
+                        Guardian
+                    </Divider>
+                    <Row gutter={20}>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="guardian.fatherName" label="Father Name" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="guardian.fatherOccupation" label="Father.Occupation" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="number" name="guardian.fatherContactNo" label="Father Contact" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="motherName" label="Mother Name" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="motherOccupation" label="Mother Occupation" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="motherContactNo" label="Mother Contact" />
+                        </Col>
+                    </Row>
+                    <Divider>
+                        Local Guardian
+                    </Divider>
+                    <Row gutter={20}>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="localGuardian.name" label="Name" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="localGuardian.occupation" label="Occupation" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="number" name="localGuardian.contactNo" label="Contact" />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="localGuardian.address" label="Address" />
+                        </Col>
+                    </Row>
+                    <Divider>
+                        Academic Info
+                    </Divider>
+                    <Row gutter={20}>
+                        <Col xs={24} sm={12} md={8}>
+                            <PhSelectForm 
+                            options={semesterOptions} 
+                            disabled={isLoading}
+                            name="admissionSemester" 
+                            label="Admission Semester"
+                             />
+                        </Col>
+                        <Col xs={24} sm={12} md={8}>
+                            <PHInputForm type="text" name="localGuardian.address" label="Address" />
+                        </Col>
                     </Row>
                     <Button htmlType="submit">Submit</Button>
                 </PhForm>
